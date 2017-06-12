ffb9416af2eb8f138a2d809ea4231f3b1ca77299
Mat Velloso <mat.velloso@microsoft.com>
2017-06-13 06:16:24 +0900

    v1

---
+++ ClownBot/CustomVisionService.cs
@@ -0,0 +1,167 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ReceiveAttachmentBot
+{
+    public class ClownScore
+    {
+        public double Nose { get; set; }
+        public double Bowtie { get; set; }
+        public double Normal { get; set; }
+        public double? Happiness { get; set; }
+    }
+    public class CognitiveServiceClient 
+    {
+        private readonly string _customVisionKey;
+        private readonly string _customVisionModel;
+        private readonly string _emotionKey;
+
+        private Uri postUri;
+
+
+        public CognitiveServiceClient(string customVisionKey, string customVisionModel, string emotionKey)
+        {
+            _customVisionKey = customVisionKey;
+            _customVisionModel = customVisionModel;
+            _emotionKey = emotionKey;
+        }
+
+        private HttpClient GetCustomVisionWebClient()
+        {
+            
+            HttpClient retVal;
+            //if (forImage)
+            //{
+                this.postUri = new Uri($@"{ConfigurationManager.AppSettings["CustomVisionURL"]}{_customVisionModel}/image");
+                retVal = new HttpClient { BaseAddress = this.postUri };
+            //}
+            //else
+            //{
+            //    this.postUri = new Uri($@"{ConfigurationManager.AppSettings["CustomVisionURL"]}{_customVisionModel}/url");
+            //    retVal = new HttpClient { BaseAddress = this.postUri };
+            //}
+
+            retVal.DefaultRequestHeaders.Add(@"Prediction-Key", _customVisionKey);
+            return retVal;
+        }
+
+        private HttpClient GetEmotionWebClient()
+        {
+
+            HttpClient retVal;
+            this.postUri = new Uri($@"https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+            retVal = new HttpClient { BaseAddress = this.postUri };
+
+            retVal.DefaultRequestHeaders.Add(@"Ocp-Apim-Subscription-Key", _emotionKey);
+            return retVal;
+        }
+
+        public async Task<ClownScore> AnalyzeAsync(Stream imageStream)
+        {
+            ClownScore clownScore = null;
+            Stream secondStream = new MemoryStream();
+            await imageStream.CopyToAsync(secondStream);
+            imageStream.Position = 0;
+            secondStream.Position = 0;
+            using (var customVisionClient = GetCustomVisionWebClient())
+            using (var content = new StreamContent(imageStream))
+            {
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/octet-stream");
+
+                var response = await customVisionClient.PostAsync(this.postUri, content);
+
+                clownScore = await ProcessCustomVisionResponse(response);
+            }
+
+            using (var emotionClient = GetEmotionWebClient())
+            using (var content = new StreamContent(secondStream))
+            {
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/octet-stream");
+
+                var response = await emotionClient.PostAsync(this.postUri, content);
+
+                var happiness = await ProcessEmotionResponse(response);
+                clownScore.Happiness = happiness;
+            }
+
+
+            return clownScore;
+        }
+
+        //public async Task<ClownScore> AnalyzeAsync(string imageUri)
+        //{
+        //    ClownScore clownScore = null;
+        //    try
+        //    {
+        //        using (var customVisionClient = GetCustomVisionWebClient())
+        //        using (var content = new StringContent($@"{{ ""url"" : ""{imageUri}"" }}"))
+        //        {
+        //            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/json");
+
+        //            var response = await customVisionClient.PostAsync(this.postUri, content);
+
+        //            clownScore = await ProcessCustomVisionResponse(response);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Diagnostics.Trace.TraceError(ex.ToString());
+        //        clownScore =  null;
+        //    }
+        //    return clownScore;
+        //}
+
+        private async Task<double?> ProcessEmotionResponse(HttpResponseMessage response)
+        {
+            List<double> scores = new List<double>();
+            if (response.IsSuccessStatusCode)
+            {
+                JArray result = JArray.Parse(await response.Content.ReadAsStringAsync());
+                foreach (JObject tag in result)
+                {
+                    scores.Add(tag["scores"]["happiness"].Value<double>());
+                }
+                if (scores.Count == 0)
+                    return null;
+                else
+                    return scores.ToArray().Sum() / scores.Count;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private async Task<ClownScore> ProcessCustomVisionResponse(HttpResponseMessage response)
+        {
+            ClownScore score = new ClownScore();
+            if (response.IsSuccessStatusCode)
+            {
+                JObject result = JObject.Parse(await response.Content.ReadAsStringAsync());
+                foreach (JObject tag in (JArray)result.Root["Predictions"])
+                {
+                    if (tag["Tag"].Value<string>()=="bowtie")
+                    {
+                        score.Bowtie = tag["Probability"].Value<double>();
+                    }else if (tag["Tag"].Value<string>() == "nose")
+                    {
+                        score.Nose = tag["Probability"].Value<double>();
+                    }else
+                    {
+                        score.Normal = tag["Probability"].Value<double>();
+                    }
+                }
+                return score;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ClownBot/ReceiveAttachmentDialog.cs
@@ -0,0 +1,75 @@
+﻿namespace ReceiveAttachmentBot
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+    using System.Configuration;
+
+    [Serializable]
+    internal class ReceiveAttachmentDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                var attachment = message.Attachments.First();
+ 
+                using (HttpClient httpClient = new HttpClient())
+                {
+
+                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+
+                    var contentLenghtBytes = responseMessage.Content.Headers.ContentLength;
+                    CognitiveServiceClient customVision = new CognitiveServiceClient(ConfigurationManager.AppSettings["CustomVisionKey"], 
+                                                                                     ConfigurationManager.AppSettings["CustomVisionModel"],
+                                                                                     ConfigurationManager.AppSettings["EmotionAPIKey"]);
+                    var result = await customVision.AnalyzeAsync(await responseMessage.Content.ReadAsStreamAsync());
+                  
+                    string response = "Let me see...";
+                    if (result.Nose > 0.8 && result.Bowtie > 0.8)
+                        response += " There is a lot of clown in you, well done.";
+                    else if (result.Nose > 0.8)
+                        response += " That is a respectable nose.";
+                    else if (result.Bowtie > 0.8)
+                        response += " Nice bowtie there";
+                    else
+                        response += " Well, there's nothing interesting about this photo that I want to comment";
+                    if (result.Happiness.HasValue)
+                    {
+                        response += " By the way, the overall happiness score of this photo is: " + Math.Round(result.Happiness.Value*100).ToString() + "%";
+                        if (result.Happiness.Value<0.5)
+                            response += " That doesn't make me happy at all";
+                        else if (result.Happiness.Value < 0.8)
+                            response += " That is a pretty ok photo";
+                        else
+                            response += " OMG THERE'S SO MUCH HAPPINESS HERE THAT I CAN'T EVEN";
+                        
+
+                    }
+
+
+                    await context.PostAsync( response);
+
+                }
+            }
+            else
+            {
+                await context.PostAsync("Less talk, more photos. Send me your photo and let me judge your clown factor.");
+            }
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+    }
+}
